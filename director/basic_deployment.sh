946793f8a82cf0b80acca6bb590f1e497feafe3b
udis <shkalim99@gmail.com>
2015-07-20 21:33:13 +0300

    opensyack deploy command error fixed

--- director/basic_deployment.sh
+++ director/basic_deployment.sh
@@ -15,6 +15,6 @@ openstack baremetal introspection bulk start
 openstack flavor create --id auto --ram 5120 --disk 40 --vcpus 2 baremetal
 openstack flavor set --property "cpu_arch"="x86_64" --property "capabilities:boot_option"="local" baremetal
 puid=`openstack management plan list | grep overcloud | awk '{print $2}'`
-openstack overcloud deploy --plan-uuid $puid  --control-scale 1 --compute-scale 2 --ceph-storage-scale 1 --block-storage-scale 0 --swift-storage-scale 0
+openstack overcloud deploy --plan $puid  --control-scale 1 --compute-scale 2 --ceph-storage-scale 1 --block-storage-scale 0 --swift-storage-scale 0
 
 
