16b81847ba38ae0a4507bf4654aa32aa0d284927
udis <shkalim99@gmail.com>
2015-07-21 16:07:57 +0300

    Added DNS configuration in overcloud deployment

--- director/basic_deployment.sh
+++ director/basic_deployment.sh
@@ -14,6 +14,8 @@ openstack baremetal configure boot
 openstack baremetal introspection bulk start
 openstack flavor create --id auto --ram 5120 --disk 40 --vcpus 2 baremetal
 openstack flavor set --property "cpu_arch"="x86_64" --property "capabilities:boot_option"="local" baremetal
+nid=`neutron subnet-list | egrep -o "\w+\-\w+\-\w+\-\w+\-\w+"`
+neutron subnet-update ${nid} --dns-nameserver 10.35.28.28
 puid=`openstack management plan list | grep overcloud | awk '{print $2}'`
 openstack overcloud deploy --plan $puid  --control-scale 1 --compute-scale 2 --ceph-storage-scale 1 --block-storage-scale 0 --swift-storage-scale 0
 
